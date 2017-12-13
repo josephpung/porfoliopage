40e6b6b1f4927f0da7d3d011394128b7822c908f
josephpung <goldenpummel@live.com>
2017-12-13 23:40:11 +0800

    added particles.js

---
+++ public/assets/js/script.js
@@ -0,0 +1,6 @@
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+$(document).ready(function(){
+   $('.parallax').parallax()
+ });
